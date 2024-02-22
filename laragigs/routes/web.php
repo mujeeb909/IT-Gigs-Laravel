78804f7d01fbd6de6448e130dda9347aa7c710fb
mujeeb909 <65164185+mujeeb909@users.noreply.github.com>
2024-02-22 16:28:37 +0500

    html layout  setting

--- laragigs/routes/web.php
+++ laragigs/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings');
 });
+// All Listings
+Route::get('/', function () {
+    return view('listings', [
+    'heading' => 'Latest Listings',
+    'listings' => Listing::all()
+    ]);
+    });
+
+    // Single Listing
+Route::get('/listings/{id}', function($id) {
+    return view('listing', [
+    'listing' => Listing::find($id)
+    ]);
+    });
